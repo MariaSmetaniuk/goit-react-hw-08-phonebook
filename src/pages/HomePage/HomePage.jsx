3df52c6a015d5e0bf9b4a24fc565a074eaf107ff
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-11-19 10:43:39 +0200

    Home and NotFound title

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,19 @@
+import { Box } from 'components/Box';
+import { useAuth } from 'hooks/useAuth';
+import { Title } from './HomePage.styled';
+
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  const { isLoggedIn } = useAuth();
+  return (
+    <Box p={5} as="main">
+      <Title>
+        {'Welcome to the Phonebook! '}
+        {isLoggedIn
+          ? 'Save all your contacts here.'
+          : 'Please, sing up to continue:)'}
+      </Title>
+    </Box>
+  );
 };
 
 export default HomePage;
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,12 @@
+import { Box } from 'components/Box';
+import { Title } from './NotFoundPage.styled';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <Box p={5} as="main">
+      <Title>Page is not found :(</Title>
+    </Box>
+  );
 };
 
 export default NotFoundPage;
